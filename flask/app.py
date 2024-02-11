e4861f1dde647dadc27cebaa0477b781e79e50d2
toni-akintola <oakintol@nd.edu>
2024-02-10 21:43:06 -0500

    flask debug

--- flask/app.py
+++ flask/app.py
@@ -11,14 +11,14 @@ countries = [
 ]
 
 
-# @app.after_request
-# def after_request(response):
-#   response.headers.add('Access-Control-Allow-Origin', '*')
-#   response.headers.add('Access-Control-Allow-Headers',
-#                        'Content-Type,Authorization')
-#   response.headers.add('Access-Control-Allow-Methods',
-#                        'GET,PUT,POST,DELETE,OPTIONS')
-#   return response
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers',
+                       'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods',
+                       'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 
 @app.get("/countries")

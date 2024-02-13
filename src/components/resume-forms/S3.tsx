d6d4f2d5e2a107a3ec20bbf921d343b9e66adec0
toni-akintola <oakintol@nd.edu>
2024-02-12 20:49:45 -0500

    hotfix to textarea bug

--- src/components/resume-forms/S3.tsx
+++ src/components/resume-forms/S3.tsx
@@ -261,7 +261,6 @@ export default function S3() {
                                     className="block w-full p-2 rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     defaultValue={""}
                                     required
-                                    value={experience.duties}
                                     onChange={(
                                         e: React.FormEvent<HTMLTextAreaElement>
                                     ) => {

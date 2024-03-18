4ec752941823f709ef496dfe2c97d1f163c60e7c
toni-akintola <oakintol@nd.edu>
2024-03-18 13:15:17 -0400

    languages dropdown

--- src/components/landing/Banner.tsx
+++ src/components/landing/Banner.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link"
 
 import { cn } from "@/lib/utils"
-import { WaypointsIcon } from "lucide-react"
+import { Copy, WaypointsIcon } from "lucide-react"
 import { motion } from "framer-motion"
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu"
+import React from "react"
 export function MainNav({
     className,
     ...props
@@ -24,7 +26,7 @@ export function MainNav({
                     </h1>
                 </motion.a>
             </div>
-            <div className="flex space-x-12">
+            <div className="flex space-x-12 items-center">
                 <motion.a
                     href="#"
                     whileHover={{ scale: 1.1 }}
@@ -41,6 +43,43 @@ export function MainNav({
                 >
                     About
                 </motion.a>
+                <motion.div whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}>
+                  <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="text-black font-bold">Languages</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Beautifully designed components that you can copy and
+                      paste into your apps. Accessible. Customizable. Open
+                      Source.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href="/docs/installation" title="Installation">
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Typography">
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+    </motion.div>
             </div>
 
             <div className="lg:flex lg:flex-1 lg:justify-end">
@@ -56,3 +95,29 @@ export function MainNav({
         </nav>
     )
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"

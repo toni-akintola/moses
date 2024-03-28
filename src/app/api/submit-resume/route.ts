e6ecc87f83c28fcda6e5c223ece4ef4505d5c7f5
toni-akintola <oakintol@nd.edu>
2024-03-28 11:17:27 -0400

    added 10 most likely career areas

--- src/app/api/submit-resume/route.ts
+++ src/app/api/submit-resume/route.ts
@@ -1,7 +1,7 @@
-import { authorizationStatusAtom } from "@/utils/atoms"
 import { createClient } from "@/utils/supabase/server"
 import { ResumeSubmission } from "@/utils/types"
 import { NextResponse } from "next/server"
+import OpenAI from "openai"
 export async function POST(request: Request) {
     const data: ResumeSubmission = await request.json()
     console.log(data)
@@ -19,6 +19,33 @@ export async function POST(request: Request) {
         authorizationstatus: data.authorizationStatus,
     })
 
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    })
+
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "user",
+                content: `Give me the 10 most likely career areas for a job applicant with this information:
+    age: ${data.age},
+    name: ${data.name},
+    number: ${data.number},
+    email: ${data.email},
+    proficiency: ${data.proficiency},
+    educations: ${data.educations},
+    skills: ${data.skills},
+    certificates: ${data.certificates},
+    authorizationStatus: ${data.authorizationStatus},
+
+    Format them in a JSON response please.
+   `,
+            },
+        ],
+        temperature: 0.7,
+    })
+    console.log(response.choices[0].message)
     console.log(error)
     return NextResponse.json({ error })
 }

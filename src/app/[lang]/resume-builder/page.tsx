8393a696633c40e1be5c438582fb707ad7919255
toni-akintola <oakintol@nd.edu>
2024-04-29 16:46:24 -0400

    translation deployment fix

--- src/app/[lang]/resume-builder/page.tsx
+++ src/app/[lang]/resume-builder/page.tsx
@@ -1,12 +1,23 @@
-import { S1 } from "@/components/resume-forms/S1"
-import { useTranslations } from "next-intl"
+import { S1, S1Props } from "@/components/resume-forms/S1"
+import { useMessages } from "next-intl"
 import { unstable_setRequestLocale } from "next-intl/server"
 import React from "react"
 
 const Page = ({ params }: { params: { lang: string } }) => {
     unstable_setRequestLocale(params.lang)
-    const t = useTranslations("s1")
-    return <S1 />
+    const messages = useMessages()
+    const S1Content = messages.s1 as unknown as S1Props
+    return (
+        <S1
+            backButton={S1Content.backButton}
+            nextButton={S1Content.nextButton}
+            email={S1Content.email}
+            phoneNumber={S1Content.phoneNumber}
+            proficiency={S1Content.proficiency}
+            name={S1Content.name}
+            age={S1Content.age}
+        />
+    )
 }
 
 export default Page

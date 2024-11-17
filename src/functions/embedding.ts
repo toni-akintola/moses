687c9d5396e2650a859180331b52297ed462462a
toni-akintola <oakintol@nd.edu>
2024-11-17 00:11:00 -0500

    spinning my wheels...

--- src/functions/embedding.ts
+++ src/functions/embedding.ts
@@ -1,6 +1,7 @@
 import OpenAI from "openai"
+
 export async function vectorize(data: string) {
-    const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+    const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! })
 
     const embedding = await client.embeddings.create({
         model: "text-embedding-3-small",

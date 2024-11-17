84c546cb946c3484a78e338cff5a761d3687e3d9
toni-akintola <oakintol@nd.edu>
2024-11-16 22:47:38 -0500

    porting over to nextjs 15

--- src/functions/embedding.ts
+++ src/functions/embedding.ts
@@ -1,7 +1,7 @@
 import OpenAI from "openai"
 export async function vectorize(data: string) {
-    const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
-
+    const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! })
+    console.log(client.apiKey)
     const embedding = await client.embeddings.create({
         model: "text-embedding-3-small",
         input: JSON.stringify(data),
